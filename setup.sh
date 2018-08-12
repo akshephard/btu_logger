ae071d464d3217ff303518afa1445d192d694903
[akshephard] <akshephard@ucdavis.edu>
2018-08-12 12:07:23 -0700

    modified the runscript to work with cron

--- setup.sh
+++ setup.sh
@@ -4,3 +4,9 @@ sudo apt-get install influxdb
 sudo apt-get install influxdb-client
 pip3 install influxdb
 pip3 install pandas
+sudo apt-get install python3-pandas
+sudo apt-get install python3-pip
+pip3 install configparser
+pip3 install PyYaml
+pip3 install influxdb
+influx -execute "create database local_btu"
