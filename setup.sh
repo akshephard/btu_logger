53055815464a8d2325d8c6f5993a78100f1687b8
[akshephard] <akshephard@ucdavis.edu>
2018-08-23 10:40:13 -0700

    added pymodbus and removed pandas from setup.sh

--- setup.sh
+++ setup.sh
@@ -3,10 +3,11 @@
 sudo apt-get install influxdb
 sudo apt-get install influxdb-client
 pip3 install influxdb
-pip3 install pandas
+pip3 install pymodbus
 sudo apt-get install python3-pandas
 sudo apt-get install python3-pip
 pip3 install configparser
 pip3 install PyYaml
 pip3 install influxdb
 influx -execute "create database local_btu"
+influx -execute "create database remote_btu"
