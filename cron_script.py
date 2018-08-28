b0dd484707913851c7551c4a1d8008ad9b5130cf
[akshephard] <akshephard@ucdavis.edu>
2018-08-28 15:44:59 -0700

    added commandline argument for config file

--- cron_script.py
+++ cron_script.py
@@ -1,30 +1,47 @@
 from local_db import Influx_Database_class
 from modbus_driver import Modbus_Driver
 import time
+import argparse
 #TODO Make this script take a command line argument for config file
 
-config_file = 'config_template.yaml'
-obj = Modbus_Driver(config_file)
-obj.initialize_modbus()
 
-local_db = Influx_Database_class(config_in=config_file, config_type="local")
-remote_db = Influx_Database_class(config_in=config_file, config_type="remote")
+def main():
+    # read arguments passed at .py file call
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", help="config file")
+
+    args = parser.parse_args()
+    config_file = args.config
+
+    #config_file = 'config_template.yaml'
+    obj = Modbus_Driver(config_file)
+    obj.initialize_modbus()
+
+    local_db = Influx_Database_class(config_in=config_file, config_type="local")
+    remote_db = Influx_Database_class(config_in=config_file, config_type="remote")
 
 
-data = obj.get_data()
-print(data)
-local_db.push_json_to_db(data=data)
-#time.sleep(1)
+    data = obj.get_data()
+    print(data)
+    local_db.push_json_to_db(data=data)
+    #time.sleep(1)
 
-time_now = time.time()
-local_data = local_db.read_from_db(time_now=time_now)
-print("length of data in local db:", len(local_data))
+    time_now = time.time()
+    local_data = local_db.read_from_db(time_now=time_now)
+    print("length of data in local db:", len(local_data))
 
-remote_db.push_df_to_db(df=local_data)
-local_db.successful_push()
-remote_data = remote_db.read_from_db(time_now=time_now)
-print("length after remote push = ",len(remote_data))
+    remote_db.push_df_to_db(df=local_data)
+    local_db.successful_push()
+    remote_data = remote_db.read_from_db(time_now=time_now)
+    print("length after remote push = ",len(remote_data))
 
-        #local_db.delete_from_db(time_now=time_now)
-        #local_data2 = local_db.read_from_db(time_now=time_now)
-        #print("length after deleting from local = ", len(local_data2))
+            #local_db.delete_from_db(time_now=time_now)
+            #local_data2 = local_db.read_from_db(time_now=time_now)
+            #print("length after deleting from local = ", len(local_data2))
+
+
+
+    return
+
+if __name__ == "__main__":
+    main()
--- run_script.py
+++ run_script.py
@@ -1,31 +1,48 @@
 from local_db import Influx_Database_class
 from modbus_driver import Modbus_Driver
 import time
+import argparse
 
-obj = Modbus_Driver('config_template.yaml')
-obj.initialize_modbus()
 
-local_db = Influx_Database_class(config_in='config_template.yaml',config_type="local")
-remote_db = Influx_Database_class(config_in='config_template.yaml',config_type="remote")
+def main():
+    # read arguments passed at .py file call
+    parser = argparse.ArgumentParser()
+    parser.add_argument("config", help="config file")
+    args = parser.parse_args()
 
-i=0
-while True:
-    data = obj.get_data()
-    print(data)
-    local_db.push_json_to_db(data=data)
-    time.sleep(1)
+    config_file = args.config
+    #config_file = 'config_template.yaml'
+    obj = Modbus_Driver(config_file)
+    obj.initialize_modbus()
 
-    if i%10 == 0 and i!=0:
-        time_now = time.time()
-        local_data = local_db.read_from_db(time_now=time_now)
-        print("length of data in local db:", len(local_data))
+    local_db = Influx_Database_class(config_in=config_file, config_type="local")
+    remote_db = Influx_Database_class(config_in=config_file, config_type="remote")
 
-        remote_db.push_df_to_db(df=local_data)
-        local_db.successful_push()
-        remote_data = remote_db.read_from_db(time_now=time_now)
-        print("length after remote push = ",len(remote_data))
+    i=0
+    while True:
+        data = obj.get_data()
+        print(data)
+        local_db.push_json_to_db(data=data)
+        time.sleep(1)
 
-        #local_db.delete_from_db(time_now=time_now)
-        #local_data2 = local_db.read_from_db(time_now=time_now)
-        #print("length after deleting from local = ", len(local_data2))
-    i+=1
+        if i%10 == 0 and i!=0:
+            time_now = time.time()
+            local_data = local_db.read_from_db(time_now=time_now)
+            print("length of data in local db:", len(local_data))
+
+            remote_db.push_df_to_db(df=local_data)
+            local_db.successful_push()
+            remote_data = remote_db.read_from_db(time_now=time_now)
+            print("length after remote push = ",len(remote_data))
+
+            #local_db.delete_from_db(time_now=time_now)
+            #local_data2 = local_db.read_from_db(time_now=time_now)
+            #print("length after deleting from local = ", len(local_data2))
+        i+=1
+
+
+
+    return
+
+if __name__ == "__main__":
+    main()
