4170503d5ff90583422525b59ae0e6d76075e328
[akshephard] <akshephard@ucdavis.edu>
2018-09-17 21:10:12 -0700

    Switched to a dictionary for the tags in the config file

--- local_db.py
+++ local_db.py
@@ -8,8 +8,6 @@ from Influx_Dataframe_Client import Influx_Dataframe_Client
 import time
 import yaml
 
-# TODO CONVERT TO YAML
-
 # @author : Marco Pritoni <mpritoni@lbl.gov>
 # @author : Anand Prakash <akprakash@lbl.gov>
 
@@ -48,6 +46,7 @@ class Influx_Database_class(object):
 
 
         self.measurement_name = dbConfig[db_section]['measurement_name']
+        self.tag_dict = dbConfig[db_section]['tag_dict']
         self.tag_names = dbConfig[db_section]['tag_list']
         self.tag_values = dbConfig[db_section]['tag_values']
         self.database_name = dbConfig[db_section]['database']
@@ -56,10 +55,17 @@ class Influx_Database_class(object):
 
 
     def push_json_to_db(self, data):
+        for key in self.tag_dict:
+            print(self.tag_dict[key])
+            output[key] = self.read_discrete(self.discrete_register_dict[key][0])
+
+        '''
         tags = {}
         for i in range(len(self.tag_names)):
             tags[self.tag_names[i]] = self.tag_values[i]
+        '''
 
+        tags = self.tag_dict
         fields = data
         pushData = [
                     {
@@ -72,6 +78,14 @@ class Influx_Database_class(object):
         self.influx_obj.write_json(json=pushData, database=self.database_name)
 
     def successful_push(self):
+        '''
+        Args:
+            None
+        Returns:
+            Nothing
+        This function adds a timestamp of a successful push to the measurement
+        "Push_to_remote". This can be used to select data from to delete.
+        '''
         tags = {}
         #tags.append("127.0.0.1")
         #tags.update({'ip':"127.0.0.1"})
@@ -88,6 +102,13 @@ class Influx_Database_class(object):
         self.influx_obj.write_json(json=pushData, database=self.database_name)
 
     def push_df_to_db(self, df):
+        '''
+        Args:
+            df: Pandas dataframe in format specified
+
+        Returns:
+            Nothing
+        '''
         self.influx_obj.write_dataframe(data=df,
             tags=self.tag_names,
             fields=self.field_names,
@@ -96,6 +117,14 @@ class Influx_Database_class(object):
 
     # Enter time in seconds
     def read_from_db(self, time_now=None):
+        '''
+        Args:
+            time_now (optional):the time to read data until from database. Default is
+            current time
+
+        Returns:
+            Pandas dataframe in format specified.
+        '''
         if time_now == None:
             time_now=time.time()*1000000000
 
@@ -108,6 +137,14 @@ class Influx_Database_class(object):
 
     # Enter time in seconds
     def delete_from_db(self, time_now=None):
+        '''
+        Args:
+            time_now (optional): The time to delete data until from database.
+            Default is current time
+
+        Returns:
+            Nothing
+        '''
         if time_now == None:
             time_now=time.time()*1000000000
 
